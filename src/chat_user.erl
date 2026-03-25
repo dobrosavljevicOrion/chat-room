3f178866f42d07fe7a6381d31f4e589e6d7ad755
Nikola Dobrosavljevic <nikola.dobrosavljevic@mera.com>
2026-03-25 01:22:41 +0100

    Add chat user loop for join notifications

--- src/chat_user.erl
+++ src/chat_user.erl
@@ -1,3 +1,24 @@
 -module(chat_user).
-
 -include("../include/chat.hrl").
+-compile(export_all).
+
+start(Name) ->
+    spawn(?MODULE, init, [Name]).
+
+init(Name) ->
+    case chat_server:join(Name) of
+        {ok, joined} ->
+            io:format("User ~s joins.~n", [Name]),
+            loop(Name);
+        {error, username_taken} ->
+            io:format("Username ~s is already taken.~n", [Name]);
+        {error, timeout} ->
+            io:format("Join timed out for ~s.~n", [Name])
+    end.
+
+loop(Name) ->
+    receive
+        {user_joined, OtherName} ->
+            io:format("Server tells User ~s: User ~s has joined the room.~n", [Name, OtherName]),
+            loop(Name)
+    end.
